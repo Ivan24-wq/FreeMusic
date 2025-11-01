280e1ca3219165fa8d8a288a7173a87deec6e113
Ivan24-wq <iv8747583@gmail.com>
2025-11-01 15:43:43 +0300

    fixed bugs

--- Player/MainPage.xaml.cs
+++ Player/MainPage.xaml.cs
@@ -44,7 +44,7 @@ public partial class MainPage : ContentPage
 
 
 	private async void Register(object sender, EventArgs e)
-	{
+	{ 
 		await Navigation.PushAsync(new SecondPage("Регистрация"));
 	}
 }
